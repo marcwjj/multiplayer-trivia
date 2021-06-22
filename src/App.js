5b8234a3f9e39f1c000409442f36f17e575b17e9
Jiajing Wang <jjwang@google.com>
2021-06-21 21:39:00 -0700

    initial button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import Button from '@material-ui/core/Button';
 
 function App() {
+  console.log(firebase);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Button variant="contained" color="primary" onClick={()=>{console.log('foo');}}> 
+          Sign in to Google 
+        </Button>
     </div>
   );
 }
